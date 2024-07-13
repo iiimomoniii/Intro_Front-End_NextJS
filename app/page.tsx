7496d0e8b47e9d95ca5435574ae788df50a22e5e
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2024-07-13 18:56:08 +0700

    Update
    Example interactive

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 
+// 2. จะต้องมาประกาศ "use client" 
+// ถ้าไม่ประกาศบน browser ไม่ไม่มีการแสดง ปุ่ม Button2 หรือ Unhandled Runtime Error
+
+"use client";
 import React from 'react'
 import RootPageClient from './_components/root/RootPageClient'
 
@@ -6,7 +10,15 @@ type Props = {}
 
 export default function Page({}: Props) {
   return (
-    // RSC Payload ใส่ 1234 เข้าไปด้วย
-    <div><RootPageClient text="1234"/></div> 
+   
+    
+    <div>
+      {/*0. สร้าง ปุ่มขึ้นมา */}
+       <button>Button</button>
+       {/* 1. แต่ถ้าปุ่มมีการ interactive เช่น onClick  */}
+       <button onClick={()=>{}}>Button2</button>
+       {/* RSC Payload ใส่ 1234 เข้าไปด้วย */}
+      <RootPageClient text="1234"/>
+    </div> 
   )
 }
